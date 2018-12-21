6eb1b9135078b206bd98548197f842733fcf1d10
MaxouSenpai <Maxime.Hauwaert@ulb.ac.be>
2018-12-21 02:30:38 +0100

    Tests added

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ void ParcoursArbre (BinarySearchTree<K,V> &tree)
 
 int main()
 {
-
+  std::cout << "\nVecteurCreux <int> a\n" << std::endl;
   VecteurCreux<int> a (10);
 
   a.set(3,11);
@@ -34,7 +34,6 @@ int main()
   a.set(8,0);
   a.set(3,0);
 
-  std::cout << "\nVecteurCreux a\n" << std::endl;
   std::cout << "Comme tableau :\n" << std::endl;
   ParcoursTableau(a);
   std::cout << "\nComme arbre :\n" << std::endl;
@@ -42,19 +41,19 @@ int main()
   std::cout << std::endl;
 
   VecteurCreux<int> b (a);
-  std::cout << "*DeepCopy de a dans b*" << std::endl;
+  std::cout << "DeepCopy de a pour b" << std::endl;
 
+  std::cout << "Modification de certains éléments de b" << std::endl;
   b.set(7,42);
   b.set(3,55);
-  std::cout << "*Modification de certains éléments de b*" << std::endl;
 
-  std::cout << "\nVecteurCreux a\n" << std::endl;
+  std::cout << "\nVecteurCreux <int> a\n" << std::endl;
   std::cout << "Comme tableau :\n" << std::endl;
   ParcoursTableau(a);
   std::cout << "\nComme arbre :\n" << std::endl;
   ParcoursArbre(a);
 
-  std::cout << "\nVecteurCreux b\n" << std::endl;
+  std::cout << "\nVecteurCreux <int> b\n" << std::endl;
   std::cout << "Comme tableau :\n" << std::endl;
   ParcoursTableau(b);
   std::cout << "\nComme arbre :\n" << std::endl;
@@ -71,6 +70,37 @@ int main()
   {
     std::cout << "Error caught : out of range\n" << std::endl;
   }
+
+  std::cout << "BinarySearchTree <long int,float> x\n" << std::endl;
+  BinarySearchTree<long int,float> x;
+
+  x.Insert(new Node<long int,float>(12413,1.23f));
+  x.Insert(new Node<long int,float>(2453,65.3f));
+  x.Insert(new Node<long int,float>(124236,246.0f));
+
+  ParcoursArbre(x);
+
+  std::cout << "\nVecteur <double> y\n" << std::endl;
+  Vecteur<double> *y = new VecteurCreux<double>(10);
+
+  y->set(2,4.2);
+  y->set(9,3.74);
+  y->set(2,43.5);
+
+  ParcoursTableau(*y);
+  delete y;
+  std::cout << std::endl;
+
+  Node<char,int> i ('a',76), j ('z',8);
+  std::cout << "Le Node ( " << i.getKey() << " | " << i.getValue() << " ) est ";
+
+  if (i<j)
+    std::cout << "plus petit";
+
+  else
+  std::cout << "plus grand";
+
+  std::cout << " que le Node ( " << j.getKey() << " | " << j.getValue() << " )\n" << std::endl;
 
   return 0;
 }
